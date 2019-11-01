Fixed masking bug, added several new conditional expectation parameterizations

causalchains/models/conditional_models.py
@@ -5,6 +5,9 @@ import torch
 import torch.nn as nn
 import numpy as np
 import math
+import logging
+import causalchains.utils.data_utils as du
+from causalchains.utils.data_utils import PAD_TOK
 from collections import namedtuple
 
 EmptyEncoder = namedtuple('EmptyEncoder', ['output_dim'])
@@ -12,13 +15,12 @@ dummy = EmptyEncoder(0)
 
 
 class ExpectedOutcome(nn.Module):
-    'Models E[e2 | e1, e1_text]'
+    'Models E[e2 | e1, e1_text], Use Embeddings for event representation'
 
-    def __init__(self, event_embeddings, text_embeddings, event_encoder, text_encoder, hidden_dim=None):
+    def __init__(self, event_embeddings, text_embeddings, event_encoder, text_encoder, evocab, tvocab, config):
         """
-        Naive expected outcome, dont adjust for previous events before e1 (M)
         Params:
-            (Torch.nn.Embeddings) event_embeddings : Pytorch nn.Embeddings Module for events
+            (Torch.nn.Embeddings) event_embeddings : Pytorch nn.Embeddings Module for events (Pass in None for no event embeddings)
             (Torch.nn.Embeddings) text_embeddings : Pytorch nn.Embeddings Module for text
             (Torch.nn.Module) event_encoder : A module for encodeing events (pass None if not using prev events)
             (Torch.nn.Module) text_encoder : A module for encodeing text 
@@ -30,18 +32,33 @@ class ExpectedOutcome(nn.Module):
         self.text_embeddings = text_embeddings
         self.text_encoder = text_encoder
         self.event_encoder = event_encoder if event_encoder is not None else dummy
+        if event_encoder is None:
+            logging.info("Not using Event Encoder for Previous Events, Thus, not using Previous Events as input")
 
-        self.event_embed_dim = self.event_embeddings.weight.shape[1]
+        self.event_embed_dim = self.event_embeddings.weight.shape[1] if self.event_embeddings is not None else 0
         self.text_embed_dim = self.text_embeddings.weight.shape[1]
-        self.num_events = self.event_embeddings.weight.shape[0]
-        self.hidden_dim = self.event_embed_dim if hidden_dim is None else hidden_dim
+        self.num_events = len(evocab.itos)
+        self.hidden_dim = self.event_embed_dim if config.mlp_hidden_dim == 0 else config.mlp_hidden_dim
 
-        mlp_input_dim = self.event_embed_dim + self.text_encoder.output_dim + self.event_encoder.output_dim
-        self.logits_mlp = nn.Sequential(
-                nn.Linear(mlp_input_dim, self.hidden_dim),
-                nn.ReLU(),
-                nn.Linear(self.hidden_dim, self.num_events)
-            )
+        self.e_pad = evocab.stoi[PAD_TOK]
+        self.t_pad = tvocab.stoi[PAD_TOK]
+        self.combine_events = config.combine_events
+
+        if self.combine_events: #Dont give seperate position to e1, treat it like previous context
+            logging.info("ExpectedOutcome: No unique position for e1, combine with prev")
+            mlp_input_dim = self.text_encoder.output_dim + self.event_encoder.output_dim
+        else:
+            mlp_input_dim = self.event_embed_dim + self.text_encoder.output_dim + self.event_encoder.output_dim
+
+        if self.event_embeddings is not None:
+            self.logits_mlp = nn.Sequential(
+                    nn.Linear(mlp_input_dim, self.hidden_dim),
+                    nn.ReLU(),
+                    nn.Linear(self.hidden_dim, self.num_events)
+                )
+        else:
+            logging.info("Not using Event Embeddings, Thus, Using One Hot Features for Events in Conditional Expectation Model")
+            self.logits_mlp = nn.Linear(mlp_input_dim, self.num_events) #Not really a MLP, but eh
 
 
     def forward(self, input):
@@ -55,13 +72,27 @@ class ExpectedOutcome(nn.Module):
             logits for e2 prediction, Tensor of [batch X num events]
         """
 
-        e1 = self.event_embeddings(input.e1) #[batch, embd_size]
         e1_text = self.text_embeddings(input.e1_text[0]) #[batch, toklength, embd size]
-#        print(e1_text)
-        encoded_text = self.text_encoder(e1_text, mask=None) #[batch, outputsize], mask not needed if pad was passed to embeddings, which it is
-        if self.event_encoder.output_dim > 0:
-            encoded_events = self.event_encoder(input.e1prev_intext[0], input.e1prev_intext[1])
-            mlp_input = torch.cat([e1, encoded_text, encoded_events], dim=1)
+        text_mask = du.create_mask(input.e1_text[0], input.e1_text[1])
+        encoded_text = self.text_encoder(e1_text, mask=text_mask) #[batch, outputsize] 
+
+        if self.includes_e1prev_intext():
+            if self.onehot_events():
+                e1 = self.event_encoder(input.e1.unsqueeze(dim=1), None) #batch x vocsize   #Get one hot encodings
+                e1prev = self.event_encoder(input.e1prev_intext[0], None) #batch x vocsize
+                events = e1 + e1prev
+                events = torch.where(events < 2, events, torch.ones(events.shape, dtype=events.dtype, device=events.device)) #make sure all features are binary (not greater than 2)
+                mlp_input = torch.cat([events, encoded_text], dim=1)
+            elif self.combine_events:
+                combined_events = torch.cat([input.e1.unsqueeze(-1), input.e1prev_intext[0]], dim=1)
+                event_mask = du.create_mask(combined_events, input.e1prev_intext[1]+1)
+                encoded_events = self.event_encoder(self.event_embeddings(combined_events), input.e1prev_intext[1], event_mask)
+                mlp_input = torch.cat([encoded_text, encoded_events], dim=1)
+            else: #Regular avg encoder
+                e1 = self.event_embeddings(input.e1) #[batch, embd_size]
+                event_mask = du.create_mask(input.e1prev_intext[0], input.e1prev_intext[1])
+                encoded_events = self.event_encoder(self.event_embeddings(input.e1prev_intext[0]), input.e1prev_intext[1], event_mask)
+                mlp_input = torch.cat([e1, encoded_text, encoded_events], dim=1)
         else:
             mlp_input = torch.cat([e1, encoded_text], dim=1)
 
@@ -69,5 +100,9 @@ class ExpectedOutcome(nn.Module):
 
     def includes_e1prev_intext(self):
         return self.event_encoder.output_dim > 0
+
+    def onehot_events(self):
+        return self.event_embeddings is None
+
 
 

causalchains/models/encoders/average_encoder.py
@@ -22,11 +22,15 @@ class AverageEncoder(nn.Module):
         self._embedding_dim = self.output_dim = embedding_dim
 
 
-    def forward(self, tokens: torch.Tensor, lengths: torch.Tensor):  # pylint: disable=arguments-differ
+    def forward(self, tokens: torch.Tensor, lengths: torch.Tensor, mask: torch.Tensor):  # pylint: disable=arguments-differ
         """
         Params:
             Tokens (Tensor[batch, maxlength, dim]) : the embeddings to avg
             lengths (Tensor[batch])
+            mask (Tensor[batch, maxleng])
         """
-        avg = torch.sum(tokens, dim=1) / lengths.view(-1, 1).type(torch.FloatTensor)
+        if mask is not None:
+            tokens = tokens * mask.unsqueeze(-1).float()
+
+        avg = torch.sum(tokens, dim=1) / (lengths.view(-1, 1).type(torch.FloatTensor).to(device=tokens.device) +1) #NEED TO CHANGE LATER
         return avg

causalchains/models/estimator_model.py
@@ -13,7 +13,9 @@ import math
 import causalchains.models.conditional_models as cmodels
 from causalchains.models.encoders.cnn_encoder import CnnEncoder
 from causalchains.models.encoders.average_encoder import AverageEncoder
+from causalchains.models.encoders.onehot_encoder import OneHotEncoder
 from causalchains.utils.data_utils import PAD_TOK
+import logging
 
 
 EXP_OUTCOME_COMPONENT="_exp_outcome_component_"
@@ -21,8 +23,10 @@ PROPENSITY_COMPONENT="_propensity_component_"
 
 
 class Estimator(nn.Module):
-    def __init__(self, event_embed_size, text_embed_size, event_encoder_outsize, text_encoder_outsize, evocab, tvocab):
+    def __init__(self, event_embed_size, text_embed_size, event_encoder_outsize, text_encoder_outsize, evocab, tvocab, config):
         super(Estimator, self).__init__()
+
+        #Pass in None for event_embed_size to just use one hot encodings for events (make sure event_encoder_outsize is the correct size)
 
         self.event_embed_size = event_embed_size
         self.text_embed_size = text_embed_size
@@ -34,11 +38,22 @@ class Estimator(nn.Module):
         e_pad = evocab.stoi[PAD_TOK]
         t_pad = tvocab.stoi[PAD_TOK]
 
-        self.event_embeddings = nn.Embedding(evocab_size, self.event_embed_size, padding_idx=e_pad)
+        if self.event_embed_size is not None:
+            self.event_embeddings = nn.Embedding(evocab_size, self.event_embed_size, padding_idx=e_pad)
+        else:
+            self.event_embeddings = None
+
         self.text_embeddings = nn.Embedding(tvocab_size, self.text_embed_size, padding_idx=t_pad)
+        if config.use_pretrained:
+            logging.info("Estimator: Using Pretrained Word Embeddings")
+            self.text_embeddings.weight.data = tvocab.vectors
 
         if self.event_encoder_outsize is not None: 
-            self.event_encoder = AverageEncoder(self.event_embed_size)
+            if self.event_embed_size is not None:
+                self.event_encoder = AverageEncoder(self.event_embed_size)
+            else: 
+                assert event_encoder_outsize == len(evocab.itos), "event_encoder_outsize incorrectly specified for OneHot, should be vocab size"
+                self.event_encoder = OneHotEncoder(len(evocab.itos), pad_idx=e_pad)
         else:
             self.event_encoder = None
 
@@ -76,16 +91,17 @@ class NaiveAdjustmentEstimator(Estimator):
                                                        config.text_embed_size,
                                                        event_encoder_outsize=None,
                                                        text_encoder_outsize=config.text_enc_output,
-                                                       evocab=evocab, tvocab=tvocab)
+                                                       evocab=evocab, tvocab=tvocab, config=config)
 
         self.expected_outcome = cmodels.ExpectedOutcome(self.event_embeddings, 
                                                              self.text_embeddings, 
                                                              event_encoder=None,
                                                              text_encoder=self.text_encoder,
-                                                             hidden_dim=config.mlp_hidden_dim)
+                                                             evocab=evocab,tvocab=tvocab,
+                                                             config=config)
 
         assert not self.expected_outcome.includes_e1prev_intext()
-
+        assert not self.expected_outcome.onehot_events()
 
     def forward(self, instance):
         exp_out = self.expected_outcome(instance)
@@ -93,27 +109,52 @@ class NaiveAdjustmentEstimator(Estimator):
         
 
 class SemiNaiveAdjustmentEstimator(Estimator):
-    'Estimate ACE with Backdoor adjustment considering previous events that occured in text (but not those that didnt'
+    'Estimate ACE with Backdoor adjustment considering previous events that occured in text (but not those that didnt appear in text)'
     def __init__(self, config, evocab, tvocab):
-        super(NaiveAdjustmentEstimator, self).__init__(config.event_embed_size, 
+        super(SemiNaiveAdjustmentEstimator, self).__init__(config.event_embed_size, 
                                                        config.text_embed_size,
                                                        event_encoder_outsize=config.event_embed_size,
                                                        text_encoder_outsize=config.text_enc_output,
-                                                       evocab=evocab, tvocab=tvocab)
+                                                       evocab=evocab, tvocab=tvocab, config=config)
 
         self.expected_outcome = cmodels.ExpectedOutcome(self.event_embeddings, 
                                                              self.text_embeddings, 
                                                              event_encoder=self.event_encoder,
                                                              text_encoder=self.text_encoder,
-                                                             hidden_dim=config.mlp_hidden_dim)
+                                                             evocab=evocab,tvocab=tvocab,
+                                                             config=config)
 
 
         assert self.expected_outcome.includes_e1prev_intext()
+        assert not self.expected_outcome.onehot_events()
 
     def forward(self, instance):
         exp_out = self.expected_outcome(instance)
         return {EXP_OUTCOME_COMPONENT: exp_out}
 
+class SemiNaiveAdjustmentEstimatorOneHotEvents(Estimator):
+    'Estimate ACE with Backdoor adjustment considering previous events that occured in text (but not those that didnt appear in text)'
+    def __init__(self, config, evocab, tvocab):
+        super(SemiNaiveAdjustmentEstimatorOneHotEvents, self).__init__(None, 
+                                                       config.text_embed_size,
+                                                       event_encoder_outsize=len(evocab.itos),
+                                                       text_encoder_outsize=config.text_enc_output,
+                                                       evocab=evocab, tvocab=tvocab, config=config)
+
+        self.expected_outcome = cmodels.ExpectedOutcome(self.event_embeddings, 
+                                                             self.text_embeddings, 
+                                                             event_encoder=self.event_encoder,
+                                                             text_encoder=self.text_encoder,
+                                                             evocab=evocab,tvocab=tvocab,
+                                                             config=config)
+
+
+        assert self.expected_outcome.includes_e1prev_intext()
+        assert self.expected_outcome.onehot_events()
+
+    def forward(self, instance):
+        exp_out = self.expected_outcome(instance)
+        return {EXP_OUTCOME_COMPONENT: exp_out}
 
 
 

causalchains/train/train.py
@@ -42,14 +42,15 @@ def validation(args, val_batches, model, loss_func):
     model.eval()
 
     valid_loss = 0.0
-    for v_iteration, inst in enumerate(val_batches):
-        instance = inst.to(args.device)
-        model_outputs = model(instance) 
-        exp_outcome_out = model_outputs[EXP_OUTCOME_COMPONENT]  #[batch X num events], output predication for e2
-        exp_outcome_loss = loss_func(exp_outcome_out, instance.e2)
-        loss = exp_outcome_loss.cpu()
+    with torch.no_grad():
+        for v_iteration, inst in enumerate(val_batches):
+            instance = du.send_instance_to(inst, args.device)
+            model_outputs = model(instance) 
+            exp_outcome_out = model_outputs[EXP_OUTCOME_COMPONENT]  #[batch X num events], output predication for e2
+            exp_outcome_loss = loss_func(exp_outcome_out, instance.e2)
+            loss = exp_outcome_loss.cpu()
 
-        valid_loss += loss
+            valid_loss += loss
   
     valid_loss = valid_loss/(v_iteration+1)   
     return valid_loss
@@ -68,12 +69,22 @@ def train(args):
     logging.info("Event Vocab Loaded, Size {}".format(len(evocab.stoi.keys())))
     logging.info("Text Vocab Loaded, Size {}".format(len(tvocab.stoi.keys())))
 
+    if args.use_pretrained:
+        pretrained = Glove(name='6B', dim=args.text_embed_size, unk_init=torch.Tensor.normal_)
+        tvocab = du.load_vectors(pretrained)
+        logging.info("Loaded Pretrained Word Embeddings")
+
     if args.load_model:
         logging.info("Loading the Model")
         model = torch.load(args.load_model)
     else:
         logging.info("Creating the Model")
-        model = estimators.NaiveAdjustmentEstimator(args, evocab, tvocab)
+        if args.onehot_events:
+            logging.info("Model Type: SemiNaiveAdjustmentEstimatorOneHotEvents")
+            model = estimators.SemiNaiveAdjustmentEstimatorOneHotEvents(args, evocab, tvocab)
+        else:
+            logging.info("Model Type: SemiNaiveAdjustmentEstimator")
+            model = estimators.SemiNaiveAdjustmentEstimator(args, evocab, tvocab)
 
     model = model.to(device=args.device)
 
@@ -84,10 +95,18 @@ def train(args):
     else:
         logging.info("Creating the optimizer anew")
         optimizer = torch.optim.Adam(model.parameters(), lr=args.lr)
+      #  optimizer = torch.optim.Adagrad(model.parameters(), lr=args.lr)
 
     logging.info("Loading Datasets")
     min_size = model.text_encoder.largest_ngram_size #Add extra pads if text size smaller than largest CNN kernel size
-    train_dataset = du.InstanceDataset(args.train_data, evocab, tvocab, min_size=min_size) 
+
+    if args.load_pickle:
+        logging.info("Loading Train from Pickled Data")
+        with open(args.train_data, 'rb') as pfi:
+            pickled_examples = pickle.load(pfi)
+        train_dataset = du.InstanceDataset("", evocab, tvocab, min_size=min_size, pickled_examples=pickled_examples) 
+    else:
+        train_dataset = du.InstanceDataset(args.train_data, evocab, tvocab, min_size=min_size) 
     valid_dataset = du.InstanceDataset(args.valid_data, evocab, tvocab, min_size=min_size)
 
     #Remove UNK events from the e1prev_intext attribute so they don't mess up avg encoders
@@ -112,7 +131,8 @@ def train(args):
     for curr_epoch in range(args.epochs):
         prev_losses = []
         for iteration, inst in enumerate(train_batches): 
-            instance = inst.to(device)
+            instance = du.send_instance_to(inst, args.device)
+
             model.train()
             model.zero_grad()
             model_outputs = model(instance) 
@@ -140,8 +160,10 @@ def train(args):
                     logging.info("New Validation Best...Saving Model Checkpoint")  
                     best_valid_loss = new_valid_loss
                     best_epoch = curr_epoch
-                    torch.save(model, "{}.epoch_{}.loss_{:.2f}.pt".format(args.save_model, curr_epoch, best_valid_loss))
-                    torch.save(optimizer, "{}.{}.epoch_{}.loss_{:.2f}.pt".format(args.save_model, "optimizer", curr_epoch, best_valid_loss))
+                    #torch.save(model, "{}.epoch_{}.loss_{:.2f}.pt".format(args.save_model, curr_epoch, best_valid_loss))
+                    #torch.save(optimizer, "{}.{}.epoch_{}.loss_{:.2f}.pt".format(args.save_model, "optimizer", curr_epoch, best_valid_loss))
+                    torch.save(model, "{}".format(args.save_model))
+                    torch.save(optimizer, "{}_optimizer".format(args.save_model))
 
         #END OF EPOCH
         logging.info("End of Epoch {}, Running Validation".format(curr_epoch))
@@ -151,8 +173,10 @@ def train(args):
             logging.info("New Validation Best...Saving Model Checkpoint")  
             best_valid_loss = new_valid_loss
             best_epoch = curr_epoch
-            torch.save(model, "{}.epoch_{}.loss_{:.2f}.pt".format(args.save_model, curr_epoch, best_valid_loss))
-            torch.save(optimizer, "{}.{}.epoch_{}.loss_{:.2f}.pt".format(args.save_model, "optimizer", curr_epoch, best_valid_loss))
+            #torch.save(model, "{}.epoch_{}.loss_{:.2f}.pt".format(args.save_model, curr_epoch, best_valid_loss))
+            #torch.save(optimizer, "{}.{}.epoch_{}.loss_{:.2f}.pt".format(args.save_model, "optimizer", curr_epoch, best_valid_loss))
+            torch.save(model, "{}".format(args.save_model))
+            torch.save(optimizer, "{}_optimizer".format(args.save_model))
 
         if curr_epoch - best_epoch >= args.stop_after:
             logging.info("No improvement in {} epochs, terminating at epoch {}...".format(args.stop_after, curr_epoch))
@@ -164,29 +188,32 @@ def train(args):
 
 if __name__ == "__main__":
 
-    parser = argparse.ArgumentParser(description='DAVAE')
+    parser = argparse.ArgumentParser(description='Training for Nuisance Conditionals')
     parser.add_argument('--train_data', type=str)
     parser.add_argument('--valid_data', type=str)
-    parser.add_argument('--evocab', type=str, help='the event vocabulary pickle file', default='/home/nweber/CausalScripts/data/evocab_freq25')
-    parser.add_argument('--tvocab', type=str, help='the text vocabulary pickle file', default='/home/nweber/CausalScripts/data/tvocab_freq100')
-    parser.add_argument('--event_embed_size', type=int, default=32, help='size of event embeddings')
-    parser.add_argument('--text_embed_size', type=int, default=32, help='size of text embeddings')
-    parser.add_argument('--text_enc_output', type=int, default=32, help='size of output of text encoder')
-    parser.add_argument('--mlp_hidden_dim', type=int, default=32, help='size of mlp hidden layer for component models')
+    parser.add_argument('--evocab', type=str, help='the event vocabulary pickle file', default='./data/evocab_freq25')
+    parser.add_argument('--tvocab', type=str, help='the text vocabulary pickle file', default='./data/tvocab_freq100')
+    parser.add_argument('--event_embed_size', type=int, default=128, help='size of event embeddings')
+    parser.add_argument('--text_embed_size', type=int, default=128, help='size of text embeddings')
+    parser.add_argument('--text_enc_output', type=int, default=128, help='size of output of text encoder')
+    parser.add_argument('--mlp_hidden_dim', type=int, default=300, help='size of mlp hidden layer for component models')
     parser.add_argument('--lr', type=float, default=0.001, help='initial learning rate')
-    parser.add_argument('--log_every', type=int, default=200)
-    parser.add_argument('--save_after', type=int, default=500)
-    parser.add_argument('--validate_after', type=int, default=2500)
+    parser.add_argument('--log_every', type=int, default=500)
+    parser.add_argument('--validate_after', type=int, default=5000)
     parser.add_argument('--optimizer', type=str, default='adam', help='adam, adagrad, sgd')
-    parser.add_argument('--clip', type=float, default=5.0, help='gradient clipping')
+    parser.add_argument('--clip', type=float, default=10.0, help='gradient clipping')
     parser.add_argument('--epochs', type=int, default=40, help='upper epoch limit')
-    parser.add_argument('--stop_after', type=int, default=2, help='Stop after this many epochs have passed without decrease in validation loss')
+    parser.add_argument('--stop_after', type=int, default=3, help='Stop after this many epochs have passed without decrease in validation loss')
     parser.add_argument('--batch_size', type=int, default=32, metavar='N', help='batch size')
     parser.add_argument('--seed', type=int, default=11, help='random seed') 
     parser.add_argument('--cuda', action='store_true', help='use CUDA')
     parser.add_argument('-save_model', default='model_checkpoint.pt', help="""Model filename""")
     parser.add_argument('--load_model', type=str)
     parser.add_argument('--load_opt', type=str)
+    parser.add_argument('--onehot_events', action='store_true', help='Dont embed events for input, just use onehot features')
+    parser.add_argument('--combine_events', action='store_true', help='Combine e1 with previous context (average it in if using embeddings)')
+    parser.add_argument('--use_pretrained', action='store_true', help='Use pretrained glove embeddings')
+    parser.add_argument('--load_pickle', action='store_true', help='Load preprocessed (pickled) examples, is quicker')
 
 
     logging.basicConfig(level=logging.INFO)
@@ -207,6 +234,8 @@ if __name__ == "__main__":
         else:
             torch.cuda.manual_seed(args.seed)
             args.device = torch.device('cuda')
+
+            logging.info("Using GPU {}".format(torch.cuda.get_device_name(args.device)))
 
     else:
         args.device = torch.device('cpu')

causalchains/utils/data_utils.py
@@ -17,6 +17,7 @@ import torchtext.data as ttdata
 import torchtext.datasets as ttdatasets
 from torchtext.vocab import Vocab
 from collections import defaultdict, Counter
+import logging
 
 #Reserved Special Tokens
 PAD_TOK = "<pad>"
@@ -109,10 +110,31 @@ class ExtendableField(ttdata.Field):
         raise NotImplementedError
 
 
+def send_instance_to(instance, device):
+    """
+    Convert Batch object so that it goes on device(gpu/cpu)
+    """
+    instance.e1_text = (instance.e1_text[0].to(device=device), instance.e1_text[1].to(device=device))
+    instance.e1 = instance.e1.to(device=device)
+    instance.e2 = instance.e2.to(device=device)
+    instance.e1prev_intext= (instance.e1prev_intext[0].to(device=device), instance.e1prev_intext[1].to(device=device))
+    return instance
+
+def create_mask(instance, lengths):
+    """
+    Param: 
+        Tensor instance (batch x maxlen)
+        Tensor lengths (batch)
+    """
+    mask = torch.arange(instance.shape[1]).expand(lengths.shape[0], instance.shape[1]) < lengths.cpu().unsqueeze(dim=1)
+    mask = mask.float().to(device=instance.device)
+
+    return mask
+
 class InstanceDataset(ttdata.Dataset):
     'Dataset for a single training instance (event 1, event 2, text, other events)'
 
-    def __init__(self, path, event_vocab, text_vocab, min_size=5, filter_unk_events=True):
+    def __init__(self, path, event_vocab, text_vocab, min_size=5, pickled_examples=None, filter_unk_events=True):
 
         """
         Args
@@ -136,30 +158,43 @@ class InstanceDataset(ttdata.Dataset):
 
         fields = [('e1_text', e1_text), ('e1', e1), ('e2', e2), ('e1prev_intext', e1prev_intext)]
         examples = []
-        with open(path, 'r') as f:
-            for line in f:
-                json_line = json.loads(line)
-                e1_text_data = json_line['e1_text'].lower()
-                e1_data = json_line['e1']
-                e2_data = json_line['e2']
-                e1prev_intext_data = json_line['e1prev_intext']
-
-                examples.append(ttdata.Example.fromlist([e1_text_data, e1_data, e2_data, e1prev_intext_data], fields))
 
         if not filter_unk_events:
             filter_pred = None
         else:
             filter_pred = lambda inst: inst.e1 in event_vocab.stoi and inst.e2 in event_vocab.stoi
- 
-        super(InstanceDataset, self).__init__(examples, fields, filter_pred=filter_pred)
 
 
-    def example_to_batch(self, example):
+        if pickled_examples is not None:
+            super(InstanceDataset, self).__init__(pickled_examples, fields, filter_pred=None) #assume already filter, no reason to do filtering again
+
+        else:
+            with open(path, 'r') as f:
+                for line in f:
+                    json_line = json.loads(line)
+                    e1_text_data = json_line['e1_text'].lower()
+                    e1_data = json_line['e1']
+                    e2_data = json_line['e2']
+                    e1prev_intext_data = json_line['e1prev_intext']
+
+                    examples.append(ttdata.Example.fromlist([e1_text_data, e1_data, e2_data, e1prev_intext_data], fields))
+
+     
+            super(InstanceDataset, self).__init__(examples, fields, filter_pred=filter_pred)
+
+
+    def example_to_batch(self, example, device=None, multiple=False):
         """
         Convert a single InstanceDataset example into a Batch object that can be directly 
         fed into the model. Useful for interactive testing of the model
         """
-        return ttdata.Batch([example], self)
+        if not multiple:
+            example = [example]
+
+        if device is None:
+            return ttdata.Batch(example, self)
+        else:
+            return send_instance_to(ttdata.Batch(example, self), device)
 
     def print_example(self, example, print_out=False):
         output = "e1: {} | e1 text: {} | Previous events: {} | e2: {}".format(example.e1,